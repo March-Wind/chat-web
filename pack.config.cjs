0a1b56b8e1f2f894fdb35ac6606af4c439666c75
liuzhiyang <zhiyang1.liu@ximalaya.com>
2024-01-31 12:20:55 +0800

    feat: 增加编译兼容的浏览器列表,增加打包后的目录

--- pack.config.cjs
+++ pack.config.cjs
@@ -1,15 +1,17 @@
 const path = require('path');
 module.exports = {
   dev_web: {
+    name: 'chat_web',
     title: 'AI助手',
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   build_spa: {
+    name: 'chat_web',
     title: 'AI助手',
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  }
-}
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+};
